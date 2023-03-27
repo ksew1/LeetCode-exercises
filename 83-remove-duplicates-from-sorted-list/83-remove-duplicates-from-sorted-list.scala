5e55c175a03fdbce94f594db14d885baaa057ad5
Karol Sewiło <95349104+ksew1@users.noreply.github.com>
2023-03-27 23:16:59 +0200

    Time: 553 ms (85.71%), Space: 55.2 MB (100.00%) - LeetHub

---
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.scala
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(_x: Int = 0, _next: ListNode = null) {
+ *   var next: ListNode = _next
+ *   var x: Int = _x
+ * }
+ */
+object Solution {
+    def deleteDuplicates(head: ListNode): ListNode = {
+        if (head == null || head.next == null) return head
+        var p = head
+
+        while (p != null && p.next != null) {
+            if (p.x == p.next.x) {
+                p.next = p.next.next
+
+            } else {
+               p = p.next 
+            }
+            
+        }
+   
+        head
+    }
+}

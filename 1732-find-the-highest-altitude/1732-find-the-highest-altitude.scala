c60ea8af13c0eb64e8d66185c81793dec9ed2eae
Karol Sewiło <95349104+ksew1@users.noreply.github.com>
2023-06-19 17:42:20 +0200

    Time: 548 ms (7.14%), Space: 55.3 MB (71.43%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.scala
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.scala
@@ -1,11 +1,14 @@
 object Solution {
-    def largestAltitude(gain: Array[Int]): Int = {
-        var highestAltitude = 0
-        var currentAltitude = 0
-        for (g <- gain) {
-            currentAltitude += g
-            highestAltitude = highestAltitude.max(currentAltitude)
-        }
-        highestAltitude
+  def largestAltitude(gain: Array[Int]): Int = {
+    findHighest(gain, 0, 0, 0)
+  }
+
+
+  def findHighest(gain: Array[Int], i: Int, high: Int, curr: Int): Int = i match {
+    case i if (i < gain.length) => {
+      val newCurr = curr + gain(i)
+      findHighest(gain, i + 1, high.max(newCurr), newCurr)
     }
+    case _ => high
+  }
 }
